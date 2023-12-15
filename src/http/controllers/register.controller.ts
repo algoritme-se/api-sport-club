b60d8aa9b736eaa99a329d1fdf616635f97b411a
Daniel Faustino <danielofaustino@proton.me>
2023-12-15 10:43:59 -0300

    feat: add dependency inversion

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,13 +1,11 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { hash } from 'bcryptjs';
-import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
-import { registerUseCase } from '@/use-cases/register';
-import { Prisma } from '@prisma/client';
+import { RegisterUseCase } from '@/use-cases/register';
+import { PrismaUserRepository } from '../repositories/prisma/prisma-user-repository';
+
 
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-
 
   const registerBodySchema = z.object({
     name: z.string(),
@@ -15,15 +13,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     password: z.string().min(6),
     role: z.enum(['STUDENT', 'TEACHER', 'ADMIN'])
   });
-  
+
   const { name, email, password } = registerBodySchema.parse(request.body);
-  
+
   try {
-    await registerUseCase({ name, email, password });
+
+    // dependency injection of PrismaUserRepository
+    const prismaUserRepository = new PrismaUserRepository();
+    const registerUseCase = new RegisterUseCase(prismaUserRepository);
+
+    await registerUseCase.execute({ name, email, password });
   } catch (error) {
     console.error(error);
     return reply.status(400).send({ message: error });
   }
-  
+
   return reply.status(201).send({ message: 'User created successfully' });
-  }
+}
---
+++ src/http/repositories/prisma/prisma-user-repository.ts
@@ -0,0 +1,33 @@
+import { $Enums, Prisma } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { UsersRepository } from "../users-repository";
+
+
+export class PrismaUserRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    return user
+
+  }
+  async create({
+    name,
+    email,
+    password,
+    role
+  }: Prisma.UserCreateInput) {
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        role
+      }
+    })
+
+    return user
+  }
+}
