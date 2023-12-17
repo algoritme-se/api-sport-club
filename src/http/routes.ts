505a8c1ccd4df4d434be4513349329d8dc5ab17e
Daniel Faustino <danielofaustino@proton.me>
2023-12-16 21:28:50 -0300

    feat(authenticate): add user authenticate case

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import { register } from "./controllers/register.controller"
 import { prisma } from "@/lib/prisma"
+import { authenticate } from "./controllers/authenticate.controller"
 
 export async function appRoutes(app: FastifyInstance) {
   
@@ -13,4 +14,6 @@ export async function appRoutes(app: FastifyInstance) {
     return reply.status(200).send(users)
 
   })
+
+  app.post('/sessions',authenticate)
 }
